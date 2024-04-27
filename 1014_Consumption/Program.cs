293907ffbd809912f313e6c63e242d92e6b8e6ed
Hui Shan <huiishan99@gmail.com>
2024-04-27 18:26:37 +0900

    Update

---
+++ 1014_Consumption/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int distance = int.Parse(Console.ReadLine());
+double fuel = double.Parse(Console.ReadLine());
+
+double consumption = distance/fuel;
+
+Console.WriteLine("{0:F3} km/l",consumption);
---
+++ 1015_DistanceBetweenTwoPoints/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] inputs1 = Console.ReadLine().Split(new char[] { ' ' });
+double x1 = double.Parse(inputs1[0]);
+double y1 = double.Parse(inputs1[1]);
+
+string[] inputs2 = Console.ReadLine().Split(new char[] { ' ' });
+double x2 = double.Parse(inputs2[0]);
+double y2 = double.Parse(inputs2[1]);
+
+double Distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+Console.WriteLine("{0:F4}", Distance);

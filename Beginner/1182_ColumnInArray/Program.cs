119fd6bfc4ff7b8b92e3b5220503bda46be64a29
Hui Shan <huiishan99@gmail.com>
2024-05-02 17:59:49 +0900

    Update

---
+++ Beginner/1182_ColumnInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int C = int.Parse(Console.ReadLine());
+char T = char.Parse(Console.ReadLine());
+double[,] M = new double[12, 12];
+
+for (int i = 0; i < 12; i++)
+{
+    for (int j = 0; j < 12; j++)
+    {
+        M[i, j] = double.Parse(Console.ReadLine());
+    }
+}
+
+double sum = 0;
+double average = 0;
+for (int i = 0; i < 12; i++)
+{
+    sum = sum + M[i, C];
+}
+
+if (T == 'S')
+{
+    Console.WriteLine("{0:F1}", sum);
+}
+else
+{
+    average = sum / 12;
+    Console.WriteLine("{0:F1}", average);
+}
---
+++ Beginner/1183_AboveTheMainDiagonal/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+char O = char.Parse(Console.ReadLine());
+double[,] M = new double[12, 12];
+
+for (int i = 0; i < 12; i++)
+{
+    for (int j = 0; j < 12; j++)
+    {
+        M[i, j] = double.Parse(Console.ReadLine());
+    }
+}
+
+double sum = 0;
+int count = 0;
+
+for (int i = 0; i < 12; i++)
+{
+    for (int j = i + 1; j < 12; j++)
+    {
+        sum = sum + M[i, j];
+        count++;
+    }
+}
+
+if (O == 'S')
+{
+    Console.WriteLine("{0:F1}", sum);
+}
+else
+{
+    double average = sum / count;
+    Console.WriteLine("{0:F1}", average);
+}

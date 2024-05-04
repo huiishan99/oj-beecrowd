dc9ec0d429cc7385f9ac020bc1897621d5c66543
Hui Shan <huiishan99@gmail.com>
2024-05-05 01:02:27 +0900

    Update

---
+++ Beginner/1541_BuildingHouses/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input;
+
+while ((input = Console.ReadLine()) != "0")
+{
+    string[] ABC = input.Split(new[] { ' ' });
+
+    int A = int.Parse(ABC[0]);
+    int B = int.Parse(ABC[1]);
+    int C = int.Parse(ABC[2]);
+
+    int houseArea = A * B;
+    double maxLandArea = houseArea * 100 / C;
+    double landSide = Math.Sqrt(maxLandArea);
+    int truncatedLandSide = (int)Math.Truncate(landSide);
+
+    Console.WriteLine(truncatedLandSide);
+}
---
+++ Beginner/1557_SquareMatrixIII/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    int N = int.Parse(Console.ReadLine());
+    if (N == 0) break;
+
+    int[,] M = new int[N, N];
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (i == 0 && j == 0)
+            {
+                M[0, 0] = 1;
+            }
+            else if (j == 0)
+            {
+                M[i, j] = M[i - 1, 0] * 2;
+            }
+            else
+            {
+                M[i, j] = M[i, j - 1] * 2;
+            }
+        }
+    }
+
+    int maxLength = M[N - 1, N - 1].ToString().Length;
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (j == N - 1)
+            {
+                Console.Write(M[i, j].ToString().PadLeft(maxLength));
+            }
+            else
+            {
+                Console.Write(M[i, j].ToString().PadLeft(maxLength) + " ");
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}

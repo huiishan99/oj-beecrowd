4c411b1a53974f38497e45c3a6769d1d5794b255
Hui Shan <huiishan99@gmail.com>
2024-05-06 00:27:47 +0900

    Update

---
+++ Beginner/1837_Preface/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] inputs = Console.ReadLine().Split(new[] { ' ' });
+int a = int.Parse(inputs[0]);
+int b = int.Parse(inputs[1]);
+
+for (int r = 0; r < Math.Abs(b); r++)
+{
+    for (int q = -1000; q < 1000; q++)
+    {
+        if (a - r == b * q)
+        {
+            Console.WriteLine("{0} {1}", q, r);
+            break;
+        }
+    }
+}
---
+++ Beginner/1847_WelcomeToTheWinter/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] inputs = Console.ReadLine().Split(new[] { ' ' });
+int a = int.Parse(inputs[0]);
+int b = int.Parse(inputs[1]);
+int c = int.Parse(inputs[2]);
+
+if (a > b)
+{
+    if (b > c && b - c >= a - b) Console.WriteLine(":(");
+    else Console.WriteLine(":)");
+}
+
+if (a < b)
+{
+    if (b < c && c - b >= b - a) Console.WriteLine(":)");
+    else Console.WriteLine(":(");
+}
+
+if (a == b)
+{
+    if (c > b) Console.WriteLine(":)");
+    else Console.WriteLine(":(");
+}

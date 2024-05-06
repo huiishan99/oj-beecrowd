bfec74e419e468936a6ad58fb1c2ac70e9e4b86d
Hui Shan <huiishan99@gmail.com>
2024-05-06 14:11:58 +0900

    Update

---
+++ Beginner/1848_CountingCrow/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Generic;
+
+Dictionary<string, int> threeEyes = new Dictionary<string, int>()
+{
+    ["--*"] = 1,
+    ["-*-"] = 2,
+    ["-**"] = 3,
+    ["*--"] = 4,
+    ["*-*"] = 5,
+    ["**-"] = 6,
+    ["***"] = 7
+};
+
+for (int i = 0; i < 3; i++)
+{
+    string input;
+    int sum = 0;
+    while ((input = Console.ReadLine()) != "caw caw")
+    {
+        sum += threeEyes[input];
+    }
+
+    Console.WriteLine(sum);
+}
---
+++ Beginner/1858_TheonSAnswer/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = int.Parse(Console.ReadLine());
+string[] inputs = Console.ReadLine().Split(new[] { ' ' });
+int[] hits = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    hits[i] = int.Parse(inputs[i]);
+}
+
+int minHits = hits[0];
+int person = 1;
+for (int i = 0; i < n; i++)
+{
+    if (minHits > hits[i])
+    {
+        minHits = hits[i];
+        person = i + 1;
+    }
+}
+
+Console.WriteLine(person);

2c1c6dbe8d3c851698691bf3087044ab230cbced
Hui Shan <huiishan99@gmail.com>
2024-05-07 22:37:01 +0900

    Update

---
+++ Beginner/1924_VitóriaAndHerIndecision/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string course = Console.ReadLine();
+}
+
+Console.WriteLine("Ciencia da Computacao");
---
+++ Beginner/1929_Triangle/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] inputs = Console.ReadLine().Split(new[] { ' ' });
+int a = int.Parse(inputs[0]);
+int b = int.Parse(inputs[1]);
+int c = int.Parse(inputs[2]);
+int d = int.Parse(inputs[3]);
+
+int[] lenght = { a, b, c, d };
+Array.Sort(lenght);
+
+bool canFormTriangle = lenght[0] + lenght[1] > lenght[2] ||
+                       lenght[0] + lenght[1] > lenght[3] ||
+                       lenght[0] + lenght[2] > lenght[3] ||
+                       lenght[1] + lenght[2] > lenght[3];
+
+Console.WriteLine(canFormTriangle ? "S" : "N");

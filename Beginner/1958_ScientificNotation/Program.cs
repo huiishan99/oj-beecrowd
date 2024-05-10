2849a0ae095700270582227ff4c66e7d0c9a0a63
Hui Shan <huiishan99@gmail.com>
2024-05-10 11:41:05 +0900

    Update

---
+++ Beginner/1958_ScientificNotation/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string number = Console.ReadLine();
+
+if (number == "-0") Console.WriteLine("-0.0000E+00");
+else
+{
+    double inpus = double.Parse(number);
+    Console.WriteLine(inpus.ToString("+0.0000E+00;-0.0000E+00"));
+}

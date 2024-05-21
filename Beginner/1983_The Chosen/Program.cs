7ab9730be1639f451caa48bf6e99c9a04f1c244b
Hui Shan <huiishan99@gmail.com>
2024-05-21 22:02:35 +0900

    Update

---
+++ Beginner/1983_The Chosen/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int testCases = int.Parse(Console.ReadLine());
+int highestScoreStudent = 0;
+double highestScore = 0;
+
+for (int i = 0; i < testCases; i++)
+{
+    string[] inputs = Console.ReadLine().Split(new[] { ' ' });
+    int studentNumber = int.Parse(inputs[0]);
+    double studentNote = double.Parse(inputs[1]);
+
+    if (studentNote >= 8 && studentNote > highestScore)
+    {
+        highestScoreStudent = studentNumber;
+        highestScore = studentNote;
+    }
+}
+
+if (highestScoreStudent != 0)
+{
+    Console.WriteLine(highestScoreStudent);
+}
+else
+{
+    Console.WriteLine("Minimum note not reached");
+}
---
+++ Beginner/1984_ThePronalânciaPuzzle/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = Console.ReadLine();
+
+char[] charArray = input.ToCharArray();
+Array.Reverse(charArray);
+
+string reverseNumber = new string(charArray);
+
+Console.WriteLine(reverseNumber);

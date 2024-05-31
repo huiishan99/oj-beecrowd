9999f66e7018590bfa0ebc1a458ffda868fa99d4
Hui Shan <huiishan99@gmail.com>
2024-05-31 23:58:19 +0900

    Update

---
+++ Beginner/2164_FastFibonacci/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = int.Parse(Console.ReadLine());
+
+double fibonacci = (Math.Pow((1 + Math.Sqrt(5)) / 2, n) -
+                    Math.Pow((1 - Math.Sqrt(5)) / 2, n))
+                   / Math.Sqrt(5);
+
+Console.WriteLine($"{fibonacci:F1}");
---
+++ Beginner/2165_Twitting/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = Console.ReadLine();
+
+if(input.Length>140)
+    Console.WriteLine("MUTE");
+else
+    Console.WriteLine("TWEET");
+
+
+

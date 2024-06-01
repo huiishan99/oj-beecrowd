286c0c805139ff2a3713454a50d65c02e530a139
Hui Shan <huiishan99@gmail.com>
2024-06-02 00:01:30 +0900

    Update

---
+++ Beginner/2179_Parity/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = Console.ReadLine();
+
+int count = 0;
+foreach (char c in input)
+{
+    if (c == '1')
+    {
+        count++;
+    }
+}
+
+if (count % 2 != 0)
+    Console.WriteLine(input + "1");
+else
+    Console.WriteLine(input + "0");

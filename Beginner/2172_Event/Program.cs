a0525f6973d090dbba6745ecd3b38a020930b4eb
Hui Shan <huiishan99@gmail.com>
2024-06-01 22:11:43 +0900

    Update

---
+++ Beginner/2172_Event/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    string[] input = Console.ReadLine().Split(new[] { ' ' });
+    int multiple = int.Parse(input[0]);
+    int expValue = int.Parse(input[1]);
+
+    if (multiple == 0 && expValue == 0) break;
+    Console.WriteLine(multiple * expValue);
+}

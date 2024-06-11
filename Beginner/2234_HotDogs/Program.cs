c6c843320149e83f5daa153fbedfc18e57980b0f
Hui Shan <huiishan99@gmail.com>
2024-06-11 15:38:12 +0900

    Update

---
+++ Beginner/2234_HotDogs/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] input = Console.ReadLine().Split(new[] { ' ' });
+int hotDogs = int.Parse(input[0]);
+int participants = int.Parse(input[1]);
+
+double result = hotDogs * 1.0 / participants;
+
+Console.WriteLine($"{result:F2}");
---
+++ Beginner/2235_WalkingInTime/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] input = Console.ReadLine().Split(new []{' '});
+int future = int.Parse(input[0]);
+int present = int.Parse(input[1]);
+int past = int.Parse(input[2]);
+

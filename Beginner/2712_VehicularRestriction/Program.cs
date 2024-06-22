9c3d9916a199739e2f9a77db644f0a3d93486e89
Hui Shan <huiishan99@gmail.com>
2024-06-23 00:37:57 +0900

    Update

---
+++ Beginner/2712_VehicularRestriction/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    string plate = Console.ReadLine();
+    Console.WriteLine(GetRestrictionDay(plate));
+}
+
+static string GetRestrictionDay(string plate)
+{
+    if (!Regex.IsMatch(plate, @"^[A-Z]{3}-\d{4}$"))
+    {
+        return "FAILURE";
+    }
+
+    char lastDigit = plate[plate.Length - 1];
+
+    switch (lastDigit)
+    {
+        case '1':
+        case '2':
+            return "MONDAY";
+        case '3':
+        case '4':
+            return "TUESDAY";
+        case '5':
+        case '6':
+            return "WEDNESDAY";
+        case '7':
+        case '8':
+            return "THURSDAY";
+        case '9':
+        case '0':
+            return "FRIDAY";
+        default:
+            return "FAILURE";
+    }
+}
---
+++ Beginner/2715_SplittingAssignmentsI/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Generic;
+using System.Linq;
+
+while (true)
+{
+    try
+    {
+        string line = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(line)) break;
+
+        int n = int.Parse(line);
+
+        line = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(line)) break;
+
+        List<int> assignments =
+            line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+        if (assignments.Count != n) break;
+
+        long totalSum = assignments.Sum(x => (long)x);
+
+        long halfSum = 0;
+        long minDifference = totalSum;
+
+        for (int i = 0; i < assignments.Count; i++)
+        {
+            halfSum += assignments[i];
+            long remainingSum = totalSum - halfSum;
+            long difference = Math.Abs(halfSum - remainingSum);
+            minDifference = Math.Min(minDifference, difference);
+        }
+
+        Console.WriteLine(minDifference);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        break;
+    }
+}

0f06dcd3c6de968aa96730e9e5cd70ce78215da2
Hui Shan <huiishan99@gmail.com>
2024-06-23 10:37:08 +0900

    Update Program.cs

--- Beginner/2717_ElfTime/Program.cs
+++ Beginner/2717_ElfTime/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+string input;
+while ((input = Console.ReadLine()) != null)
+{
+    int N = int.Parse(input);
+    string[] secondLine = Console.ReadLine().Split(new[] { ' ' });
+    int A = int.Parse(secondLine[0]);
+    int B = int.Parse(secondLine[1]);
+
+    if (A + B > N)
+    {
+        Console.WriteLine("Deixa para amanha!");
+    }
+    else
+    {
+        Console.WriteLine("Farei hoje!");
+    }
+}
